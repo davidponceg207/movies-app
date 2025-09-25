a7ad398a3f0486a911da546d400f08903cecd375
David <davidponceg207@gmail.com>
2025-09-25 15:15:42 -0600

    Navigator [Initial configuration]

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import { View, Text } from "react-native"
+import { NavigationContainer } from "@react-navigation/native"
+import { Navigation } from "./presentation/navigation/Navigation"
+
 
 export const App = () => {
 
   return (
-    <View>
-      <Text>MoviesApp</Text>
-    </View>
+    <NavigationContainer>
+      <Navigation />
+    </NavigationContainer>
   )
 }
