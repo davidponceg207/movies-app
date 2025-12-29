05408ae69718dbb30e62f58ddd3a93f36e61846c
David <davidponceg207@gmail.com>
2025-12-29 16:22:16 -0600

    Details Screen [Initial]

--- src/presentation/screens/details/DetailsScreen.tsx
+++ src/presentation/screens/details/DetailsScreen.tsx
@@ -1,6 +1,15 @@
+import { useRoute } from "@react-navigation/native"
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { Text, View } from "react-native"
+import { RootStackParams } from "../../navigation/Navigation";
 
-export const DetailsScreen = () => {
+interface Props extends NativeStackScreenProps<RootStackParams, 'Details' >{}
+
+export const DetailsScreen = ({ route }: Props) => {
+
+    const { movieId } = route.params;
+
+    console.log({movieId});
     return (
        <View>
             <Text>Details Screen</Text>
