c60259556371d4a20e9c481367d4778404f09b35
David <davidponceg207@gmail.com>
2025-12-29 21:33:54 -0600

    UseMovie

--- src/presentation/screens/details/DetailsScreen.tsx
+++ src/presentation/screens/details/DetailsScreen.tsx
@@ -2,6 +2,7 @@ import { useRoute } from "@react-navigation/native"
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { Text, View } from "react-native"
 import { RootStackParams } from "../../navigation/Navigation";
+import { useMovie } from "../../hooks/useMovie";
 
 interface Props extends NativeStackScreenProps<RootStackParams, 'Details' >{}
 
@@ -9,7 +10,7 @@ export const DetailsScreen = ({ route }: Props) => {
 
     const { movieId } = route.params;
 
-    console.log({movieId});
+    const {} = useMovie( movieId );
     return (
        <View>
             <Text>Details Screen</Text>
