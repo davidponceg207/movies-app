3057bdc12be4b395e35443a97b58ebe46eb7cd90
David <davidponceg207@gmail.com>
2026-01-02 13:33:44 -0600

    Movie Header

--- src/presentation/screens/details/DetailsScreen.tsx
+++ src/presentation/screens/details/DetailsScreen.tsx
@@ -1,8 +1,8 @@
-import { useRoute } from "@react-navigation/native"
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { Text, View } from "react-native"
 import { RootStackParams } from "../../navigation/Navigation";
 import { useMovie } from "../../hooks/useMovie";
+import { MovieHeader } from "../../components/movie/MovieHeader";
 
 interface Props extends NativeStackScreenProps<RootStackParams, 'Details' >{}
 
@@ -10,10 +10,18 @@ export const DetailsScreen = ({ route }: Props) => {
 
     const { movieId } = route.params;
 
-    const {} = useMovie( movieId );
+    const { isLoading, movie } = useMovie( movieId );
+
+    if( isLoading ) {
+        return <Text>Loading...</Text>
+    }
     return (
        <View>
-            <Text>Details Screen</Text>
+            <MovieHeader
+                title={ movie!.title }
+                originalTitle={ movie!.originalTitle }
+                poster={ movie!.poster }
+            />
        </View>
     )
 }
