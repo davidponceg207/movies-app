644e0eb444061936269589af05c7fe7feb49d4ff
David <davidponceg207@gmail.com>
2026-01-02 16:21:40 -0600

    DotENV

--- src/config/adapters/movieDB.adapter.ts
+++ src/config/adapters/movieDB.adapter.ts
@@ -1,9 +1,11 @@
+import { THE_MOVIE_DB_KEY } from "@env";
 import { AxiosAdapter } from "./http/axios.adapter";
 
 export const movieDBFetcher = new AxiosAdapter({
     baseUrl: 'https://api.themoviedb.org/3/movie',
     params: {
-        api_key: '6c06adb9114387f2366eec134782991b',
+        // api_key: '6c06adb9114387f2366eec134782991b',
+        api_key: THE_MOVIE_DB_KEY ?? 'no-key',
         language: 'en'
     }
 })
